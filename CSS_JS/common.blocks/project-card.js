101b69a46e482b7e5e2628988f15ea2ae66e487a
Phyvie <61946637+Phyvie@users.noreply.github.com>
2025-12-10 23:05:35 +0100

    Created an okay-ish layout for the project-gallery; Need to clean up the CSS file next

--- CSS_JS/common.blocks/project-card.js
+++ CSS_JS/common.blocks/project-card.js
@@ -7,11 +7,40 @@ function SetupProjectCards()
 function SetupProjectCard(projectCard)
 {
     const infoButton = projectCard.querySelector('.info-button-icon.overlay__top-right');
-    const infoContainer = projectCard.querySelector('.overlay__right');
+    const infoContainer = projectCard.querySelector('.overlay__right--A');
 
     infoButton.addEventListener('click', () => {
         infoContainer.classList.toggle('overlay__right--inactive');
     });
 }
 
-document.addEventListener('DOMContentLoaded', SetupProjectCards);
+let projectCardTemplate;
+function FindProjectCardTemplate(){
+    projectCardTemplate = document.getElementById('project-card--template');
+}
+
+function CreateProjectCard(name) {
+    let projectCard = projectCardTemplate.content.cloneNode(true);
+    projectCard.querySelector('.project-card').classList.add(name);
+    return projectCard;
+}
+
+//+ZyKa should figure out a more modular structure for this & probably put the project-card-template into another html file
+function CreateProjectCards()
+{
+    let CardParent = document.getElementById('further-projects-section__cards-grid');
+
+    let projects = ['first-proj', 'second-proj', 'third-proj', 'fourth-proj', 'fifth-proj', 'sixth-proj']
+    for(name of projects)
+    {
+        let project = CreateProjectCard('further-projects-section__' + name);
+        CardParent.appendChild(project);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>
+    {
+        FindProjectCardTemplate();
+        CreateProjectCards();
+        SetupProjectCards();
+    });
