e035b48294a8ca50a062854bc789b835c3b3bb7b
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-01-09 01:57:16 +0100

    basically wasted a day, just to decide that I link the project-names to their grid-areas via an extra .js script; however, I installed scss & learned that it's not as useful as I hoped it would be; Next step: resetup the project-cards to be filled with data

--- CSS_JS/common.blocks/project-card.js
+++ CSS_JS/common.blocks/project-card.js
@@ -1,57 +1,27 @@
 /**
  * responsible for creating the project cards in the further-projects-section and filling them with data from the corresponding .json files
+ * 0ZyKa currently double responsibility; 1. general abstract implementation of creating project cards and 2. loading the data from the database to arrange the layout of the grid
  */
 
-let CONFIG_FURTHER_PROJECTS;
-async function loadProjectConfig()
-{
-    const response = await fetch('./././Data/Projects/further-projects-database.json');
-    CONFIG_FURTHER_PROJECTS = await response.json();
-
-    setCSSCustomProperties();
-}
-
-function setCSSCustomProperties()
-{
-
-}
-
-
 let TEMPLATE_PROJECT_CARD;
-function FindProjectCardTemplate(){
+export function FindProjectCardTemplate(){
     TEMPLATE_PROJECT_CARD = document.getElementById('template--project-card');
 }
 
-function SetupProjectCards()
+export function SetupProjectCardsInteraction()
 {
     const projectCards = document.querySelectorAll('.project-card');
-    for (const card of projectCards) {SetupProjectCard(card); }
+    for (const card of projectCards) {SetupProjectCardInteraction(card); }
 }
 
-function CreateProjectCard(name) {
-    let projectCard = TEMPLATE_PROJECT_CARD.content.cloneNode(true);
-    projectCard.querySelector('.project-card').classList.add(name);
-    return projectCard;
-}
-
-//+ZyKa should figure out a more modular structure for this & probably put the project-card-template into another html file
-function CreateProjectCards()
-{
-    let CardParent = document.getElementById('further-projects-section__cards-grid');
-
-    let projects = ['first-proj', 'second-proj', 'third-proj', 'fourth-proj', 'fifth-proj', 'sixth-proj']
-
-    for(name of projects)
-    {
-        let project = CreateProjectCard('further-projects-section__' + name);
-        CardParent.appendChild(project);
-    }
+export function CreateProjectCard() {
+    return TEMPLATE_PROJECT_CARD.content.cloneNode(true);
 }
 
 /*
  * sets up the foldout of the overlay when the user clicks on the info button
  */
-function SetupProjectCard(projectCard)
+function SetupProjectCardInteraction(projectCard)
 {
     const infoButton = projectCard.querySelector('.info-button-icon.overlay__top-right');
     const infoContainer = projectCard.querySelector('.overlay__right--A');
@@ -61,9 +31,7 @@ function SetupProjectCard(projectCard)
     });
 }
 
-document.addEventListener('DOMContentLoaded', () =>
-    {
-        FindProjectCardTemplate();
-        CreateProjectCards();
-        SetupProjectCards();
-    });
+export function LoadProjectCardData(projectCard, jsonFile)
+{
+    console.log("loading data for project card: " + projectCard.id + " from " + jsonFile + "; project-name: " + jsonFile["project-title"]);
+}
