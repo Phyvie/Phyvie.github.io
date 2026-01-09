bc7547232b6321333bbc9f5287345b5563f8b2a1
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-01-09 01:57:16 +0100

    basically wasted a day, just to decide that I link the project-names to their grid-areas via an extra .js script; however, I installed scss & learned that it's not as useful as I hoped it would be; Next step: resetup the project-cards to be filled with data

---
+++ CSS_JS/portfolio.blocks/further-projects-section-load-cards.js
@@ -0,0 +1,48 @@
+import {
+    FindProjectCardTemplate,
+    CreateProjectCard,
+    SetupProjectCardsInteraction,
+    LoadProjectCardData
+} from "../common.blocks/project-card.js";
+
+async function CreateProjectCards() {
+    let CardParent = document.getElementById('further-projects-section__cards-grid');
+
+    let projects = [
+        {folder: 'Lone_Signal', 'grid-area': 'big1'},
+        {folder: 'Music_Box', 'grid-area': 'big2'},
+        {folder: 'Play_My_Math', 'grid-area': 'small1'},
+        {folder: 'ADHD_Podcast', 'grid-area': 'small2'},
+        {folder: 'Dont_Brake', 'grid-area': 'small3'},
+        {folder: 'Bevoiced', 'grid-area': 'small4'}
+    ]
+
+    for (let project of projects) {
+        try {
+            //create the project card from the template
+            let projectCardFragment = CreateProjectCard('further-projects-section__' + project.folder);
+            let projectCard = projectCardFragment.querySelector('.project-card');
+
+            CardParent.appendChild(projectCardFragment);
+            projectCard.style.gridArea = project['grid-area'];
+
+            //find the project.json file
+            const response = await fetch(`Data/Projects/${project.folder}/project_data.json`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const jsonData = await response.json();
+
+            //!ZyKa fill the card with the data
+            LoadProjectCardData(projectCard, jsonData);
+        } catch (error) {
+            console.error(`Error loading project ${project.folder}:`, error);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    FindProjectCardTemplate();
+    await CreateProjectCards();
+    SetupProjectCardsInteraction();
+});
