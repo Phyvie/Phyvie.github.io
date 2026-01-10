b71ac5a19d9ec0c1f2c553c8af8452866cfc547f
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-01-11 00:49:34 +0100

    properly added media--hover-scale to the image, while the info-button still works; still need to adjust the video to have controls & the surrounding info to be deactivated upon starting the video

--- CSS_JS/portfolio.blocks/further-projects-section-load-cards.js
+++ CSS_JS/portfolio.blocks/further-projects-section-load-cards.js
@@ -1,8 +1,8 @@
 import {
     FindProjectCardTemplate,
     CreateProjectCard,
-    SetupProjectCardsInteraction,
-    LoadProjectCardData
+    LoadProjectCardData,
+    SetupProjectCardInteraction
 } from "../common.blocks/project-card.js";
 
 async function CreateProjectCards() {
@@ -19,21 +19,23 @@ async function CreateProjectCards() {
 
     for (let project of projects) {
         try {
-            //create the project card from the template
+            //create the project card & attach it
             let projectCardFragment = CreateProjectCard('further-projects-section__' + project.folder);
             let projectCard = projectCardFragment.querySelector('.project-card');
-
             CardParent.appendChild(projectCardFragment);
             projectCard.style.gridArea = project['grid-area'];
 
             //find the project.json file
             const response = await fetch(`Data/Projects/${project.folder}/project_data.json`);
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
+                continue;
             }
             const jsonData = await response.json();
 
+            //load the data & make the card interactive
             LoadProjectCardData(projectCard, jsonData);
+            SetupProjectCardInteraction(projectCard);
         } catch (error) {
             console.error(`Error loading project ${project.folder}:`, error);
         }
@@ -43,5 +45,4 @@ async function CreateProjectCards() {
 document.addEventListener('DOMContentLoaded', async () => {
     FindProjectCardTemplate();
     await CreateProjectCards();
-    SetupProjectCardsInteraction();
 });
