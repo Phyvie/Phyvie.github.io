70aa867c262b04377cc9458ee4a643fc958d1c44
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-01-18 16:19:59 +0100

    added DontBrake-Thumbnail & Clip, as well as Gragoon project card

--- CSS_JS/portfolio.blocks/further-projects-section-load-cards.js
+++ CSS_JS/portfolio.blocks/further-projects-section-load-cards.js
@@ -15,7 +15,7 @@ async function CreateProjectCards() {
         {folder: 'Lone_Signal', 'grid-area': 'big2'},
         {folder: 'Music_Box', 'grid-area': 'big1'},
         {folder: 'Monster_Match', 'grid-area': 'small1'},
-        {folder: 'ADHD_Podcast', 'grid-area': 'small2'},
+        {folder: 'Gragoon', 'grid-area': 'small2'},
         {folder: 'Dont_Brake', 'grid-area': 'small3'},
         {folder: 'Bevoiced', 'grid-area': 'small4'}
     ]
