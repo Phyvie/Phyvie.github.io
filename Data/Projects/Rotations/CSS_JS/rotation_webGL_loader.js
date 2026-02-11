5c1d11a524a6c37f6398b94c1d7df85e796f0b13
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-02-11 01:36:24 +0100

    added example to scroll-container.js; renamed folder from Rotation_Parameterizations to Rotations; deferred some scripts; NEXT: figure out why the WebGL loader only works on Portfolio.html, but not on Rotations.html

---
+++ Data/Projects/Rotations/CSS_JS/rotation_webGL_loader.js
@@ -0,0 +1,24 @@
+import {embedGame} from "../../../../CSS_JS/UnityWebGL/unity-webgl-iframe-creator.js";
+import {iFrameHtmlPath} from "../../../../CSS_JS/UnityWebGL/unity-webgl-iframe-creator.js";
+
+function initialize()
+{
+    embedGame(document.querySelector("#unity-webGL-container"),
+        {
+            iFramePath: iFrameHtmlPath,
+            buildPath: "./Content/Build",
+            buildName: "WebBuild",
+            companyName: "",
+            productName: "Rotation_Parameterization_Visualisation",
+            productVersion: "1.0.0",
+        })
+}
+
+if (document.readyState === 'loading')
+{
+    document.addEventListener('DOMContentLoaded', initialize);
+}
+else
+{
+    await initialize();
+}
