1a412e6d3de6e6dd6d4d8ceda3d5c154f93f6647
Phyvie <61946637+Phyvie@users.noreply.github.com>
2026-02-14 01:29:52 +0100

    fixed project_data->project-type
    hard-struggling with the sizing of the scroll-container, because the content has to be big enough to be properly viewable for the user, while the description also needs to fit somewhere;; I surrender for today
    
    still struggling with the sizing of elements inside the scroll-container; Why the fuck is overflow allowed to influence the size of elements
    
    quicksave
    
    diverse changes, because I didn't commit regularly enough
    
    - adjusted contribution layout for different screen sizes
    - project-card loads its template by itself
    - webGL config is passed directly as object via postMessage -> no longer serializing, deserializing, ... -> deleted a few functions
    - minimalizes the webGL.html template
    - alternative versions of the header-section
    - figured out Portfolio-Root-Absolute-Links
    
    quicksave, because I don't want to fiddle with the whole webGL templating bullshit anymore; I want to try whether I can simply create one WebGL-page for each project and iframe that
    
    actually somehow managed to get the WebGL to run again, but now I am really unsure whether all the engineering to have an "embedWebGL"-function was worth it or just made things more complicated
    
    added workflow_template
    
    tested the scroll-container for the project-section-media; also installed minify.js and postcss (or something like that)
    
    proper switching from video to webGL and back (via scroll-container)
    
    tested filtering system for project cards & created a reference html for the project cards
    
    fixed icon-loading via IconRegistry
    
    added the main-contribution onto the project-cards
    
    filter-tag-documentation
    
    filtering the further_project_section
    
    filter-tags can now take multiple tags
    
    project card unfoldable downwards
    
    implemented lightbox
    
    added project_data_sorter.py
    
    quicksave before normalizeProjectFiles
    
    reordered project-data and added descriptions main-contributions to projects

---
+++ CSS_JS/portfolio.blocks/further-projects-section.js
@@ -0,0 +1,94 @@
+import {TryLoadJson} from "../common.blocks/load-data-refs.js";
+
+import {
+    AppendProjectCardToElement,
+    LoadProjectCardData,
+    SetupProjectCardInteraction,
+} from "../project-card/project-card.js";
+import {GetPathFromPortfolioRoot} from "../../PortfolioRootPath.js";
+import {addFilterTagToElement, createFilterTag} from "../filter-tags/filter-tags.js";
+
+async function CreateProjectCards() {
+    let CardParent = document.getElementById('further-projects-section__cards-container');
+    if (!CardParent)
+    {
+        console.error("Failed to find further-projects-section__cards-grid element; aborting CreateProjectCards");
+        return;
+    }
+
+    let projects = [
+        {folder: 'Lone_Signal'},
+        {folder: 'Music_Box'},
+        {folder: 'Monster_Match'},
+        {folder: 'Gragoon'},
+        {folder: 'Dont_Brake'},
+        {folder: 'Bevoiced'}
+    ]
+
+    const ProjectFolderURL = GetPathFromPortfolioRoot("_./Projects/");
+    for (let project of projects) {
+        try {
+            //create the project card & attach it
+            let projectCard = AppendProjectCardToElement(CardParent);
+            projectCard.id = project.folder;
+            projectCard.classList.add('further-projects-section__project-item--flex');
+
+            //find the project.json file
+            const projectDataURL = new URL(`${project.folder}/project_data.json`, ProjectFolderURL).href;
+            const jsonData = await TryLoadJson(projectDataURL);
+            if (jsonData === null) {
+                continue;
+            }
+
+            //load the data & make the card interactive
+            LoadProjectCardData(projectCard, jsonData);
+            SetupProjectCardInteraction(projectCard);
+
+            const filterTags = jsonData.tags;
+            if (!filterTags) {
+                console.warn(`No tags found in project ${project.folder}`);
+                continue;
+            }
+            for (const project_tag of filterTags)
+            {
+                let newTag = createFilterTag(project_tag.name, project_tag.relevance);
+                if (!newTag)
+                {
+                    console.error(`Failed to create filter tag ${project_tag} for project ${project.folder}`);
+                    continue;
+                }
+                addFilterTagToElement(projectCard, newTag);
+            }
+
+            const projectCardUnfoldButton = projectCard.querySelector('[data-more-info-button]');
+            if (projectCardUnfoldButton)
+            {
+                projectCardUnfoldButton.addEventListener("click", () => {
+                    if (projectCard.style.gridRow === "span 2")
+                    {
+                        projectCard.style.gridRow = "span 1";
+                    }
+                    else
+                    {
+                        projectCard.style.gridRow = "span 2";
+                    }
+                })
+            }
+        } catch (error) {
+            console.error(`Error loading project ${project.folder}:`, error);
+        }
+    }
+}
+
+async function initializeOnce()
+{
+    await CreateProjectCards();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeOnce);
+}
+else
+{
+    initializeOnce();
+}
